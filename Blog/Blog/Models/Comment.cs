d0f74316abec71443eaec9b647ec2d37d3a95ef8
Ron <ron.naor@gmail.com>
2016-09-03 15:55:18 +0300

    name change Blog

---
+++ Blog/Blog/Models/Comment.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Blog.Models
+{
+    public class Comment
+    {
+        public int CommentID { get; set; }
+
+        public int PostID { get; set; }
+
+        [Required(ErrorMessage = "Comment must have title")]
+        [DataType(DataType.Text)]
+        [Display(Name = "Comment Title:")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Author name is required")]
+        [RegularExpression(@"([\p{L}'-]+) ([\p{L}'-]+)", ErrorMessage = "Must be only chars")]
+        [Display(Name = "Author Name:")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Webadress is required")]
+        [DataType(DataType.Url)]
+        [Display(Name = "Web adress:")]
+        public string Web { get; set; }
+
+        [Required(ErrorMessage = "Content is required")]
+        [DataType(DataType.MultilineText)]
+        [Display(Name = "Content:")]
+        public int Content { get; set; }
+
+        [Display(Name = "Post:")]
+        public Post Post { get; set; }
+    }
+}
