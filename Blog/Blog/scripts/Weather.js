1785514d511d185df833da9e05efbe2a70cf2b1e
rideler <shaykr@etoro.com>
2016-10-18 17:17:46 +0300

    weather

---
+++ Blog/Blog/scripts/Weather.js
@@ -0,0 +1,12 @@
+﻿(function () {
+    $(document).ready(function () {
+        $.getJSON("http://api.openweathermap.org/data/2.5/weather?id=293397&units=metric&APPID=e40e7a67ef72c2f7260ec5aa3c14bb0d", function (data) {
+            $(document).ready(function () {
+                $('#city').text(data.name);
+                $('#country').text(data.sys.country);
+                $('#temp').text(data.main.temp);
+                $('#humidity').text(data.main.humidity);
+            });
+        });
+    });
+}());
